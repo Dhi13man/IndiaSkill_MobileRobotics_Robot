fc9ac802028456c35b59e0a662c1127b2500d668
Dhiman Seal <furyx.ds@gmail.com>
2021-09-14 16:37:23 +0530

    Readme created and Object Orienting Pizzaz

--- src/controllers/4wheel_drive_controller.cpp
+++ src/controllers/4wheel_drive_controller.cpp
@@ -1,4 +1,4 @@
-#include "motordriver_controller.cpp"
+#include "motordriver_controllers.cpp"
 
 /// <summary>
 /// @file 4wheel_drive_controller.cpp
@@ -14,7 +14,7 @@
 /// the two wheels on the back of the robot respectively.
 class FourWheelDriveController {
 private:
-    L298Controller frontDriver, backDriver;
+    MotorDriverController *frontDriver, *backDriver;
 
     String status;
 
@@ -23,7 +23,7 @@ public:
     /// @param frontMotorDriver L298Controller object that is meant to control the front wheels of the robot.
     /// @param backMotorDriver L298Controller object that is meant to control the back wheels of the robot.
     /// @return L298Controller object
-    FourWheelDriveController(L298Controller &frontMotorDriver, L298Controller &backMotorDriver) {
+    FourWheelDriveController(MotorDriverController *frontMotorDriver, MotorDriverController *backMotorDriver) {
         frontDriver = frontMotorDriver;
         backDriver = backMotorDriver;
         status = "Ready";
@@ -32,55 +32,55 @@ public:
     /// MOVEMENT FUNCTION --> Left
     /// @param speed Speed of the left movement. Range: 0-255. Default: 255
     void smoothLeft(int speed=255){
-        frontDriver.smoothLeft(speed);
-        backDriver.smoothLeft(speed);
+        frontDriver->smoothLeft(speed);
+        backDriver->smoothLeft(speed);
         status = "Moving Smooth Left!";
     }
 
     /// MOVEMENT FUNCTIONS --> Right
     /// @param speed Speed of the right movement. Range: 0-255. Default: 255
     void smoothRight(int speed=255){
-        frontDriver.smoothRight(speed);
-        backDriver.smoothRight(speed);
+        frontDriver->smoothRight(speed);
+        backDriver->smoothRight(speed);
         status = "Moving Smooth Right!";
     }
 
     /// MOVEMENT FUNCTIONS --> On-Spot Left
     /// @param speed Speed of the left movement. Range: 0-255. Default: 255
     void hardLeft(int speed=255){
-        frontDriver.hardLeft(speed);
-        backDriver.hardLeft(speed);
+        frontDriver->hardLeft(speed);
+        backDriver->hardLeft(speed);
         status = "Moving Hard Left!";
     }
 
     /// MOVEMENT FUNCTIONS --> On-Spot Right
     /// @param speed Speed of the right movement. Range: 0-255. Default: 255
     void hardRight(int speed=255){
-        frontDriver.hardRight(speed);
-        backDriver.hardRight(speed);
+        frontDriver->hardRight(speed);
+        backDriver->hardRight(speed);
         status = "Moving Hard Right!";
     }
 
     /// MOVEMENT FUNCTIONS --> Forward
     /// @param speed Speed of the forward movement. Range: 0-255. Default: 255
     void forward(int speed=255){
-        frontDriver.forward(speed);
-        backDriver.forward(speed);
+        frontDriver->forward(speed);
+        backDriver->forward(speed);
         status = "Moving Forward!";
     }
 
     /// MOVEMENT FUNCTIONS --> Back
     /// @param speed Speed of the reverse/backwards movement. Range: 0-255. Default: 255
-    void back(int speed=255){
-        frontDriver.back(speed);
-        backDriver.back(speed);
+    void backward(int speed=255){
+        frontDriver->backward(speed);
+        backDriver->backward(speed);
         status = "Moving Backward!";
     }
 
     /// MOVEMENT FUNCTIONS --> Stop
     void stop(){
-        frontDriver.stop();
-        backDriver.stop();
+        frontDriver->stop();
+        backDriver->stop();
         status = "Stopped!";
     }
 
@@ -89,8 +89,8 @@ public:
     /// @return String containing the status of the 4 wheel bot system.
     String getStatus(bool verbose=false){
         String fullStatus = "4 Wheel Drive System Status: " + status;
-        fullStatus += ", Front Left Motor Status: " + frontDriver.getStatus(verbose=false);
-        fullStatus += ", Front Right Motor Status: " + backDriver.getStatus(verbose=false);
+        fullStatus += ", Front Left Motor Status: " + frontDriver->getStatus(verbose=false);
+        fullStatus += ", Front Right Motor Status: " + backDriver->getStatus(verbose=false);
         if(verbose) Serial.println(fullStatus);
         return fullStatus;
     }
--- src/controllers/motordriver_controllers.cpp
+++ src/controllers/motordriver_controllers.cpp
@@ -8,20 +8,63 @@
 /// @date 2021-09-14
 
 
+/// Template class for all motor driver controller Classes.
+class MotorDriverController {
+protected:
+    String status;
+
+public:
+    /// MOVEMENT FUNCTION --> Left
+    /// @param speed Speed of the right motor. Range: 0-255. Default: 255
+    virtual void smoothLeft(int speed);
+
+    /// MOVEMENT FUNCTIONS --> Right
+    /// @param speed Speed of the left motor. Range: 0-255. Default: 255
+    virtual void smoothRight(int speed);
+
+    /// MOVEMENT FUNCTIONS --> On-Spot Left
+    /// @param speed Speed of the right motor forward and left motor reversed. Range: 0-255. Default: 255
+    virtual void hardLeft(int speed);
+
+    /// MOVEMENT FUNCTIONS --> On-Spot Right
+    /// @param speed Speed of the left motor forward and right motor reversed. Range: 0-255. Default: 255
+    virtual void hardRight(int speed);
+
+    /// MOVEMENT FUNCTIONS --> Forward
+    /// @param speed Speed of both motors. Range: 0-255. Default: 255
+    virtual void forward(int speed);
+    
+    /// MOVEMENT FUNCTIONS --> Backward
+    /// @param speed Speed of both motors in reverse. Range: 0-255. Default: 255
+    virtual void backward(int speed);
+
+    /// MOVEMENT FUNCTIONS --> Stop
+    virtual void stop();
+
+    /// GETTER FUNCTION --> Status
+    /// @return [String] status of the Motor Driver.
+    /// @param verbose [bool] if true, prints the status of the motor driver in Serial.
+    virtual String getStatus(bool verbose = false) {
+        if (verbose) Serial.println(status);
+        return status;
+    }
+};
+
+
 /// @class L298Controller
 /// @brief Class that is solely responsible for controlling a L298N Motor driver.
 ///
 /// @details Initialize with [leftForwardPin, leftBackwardPin, rightForwardPin, rightBackwardPin]
 /// [enableLeftPin, enableRightPin] are optional for speed control.
-class L298Controller {
-    private:
+class L298Controller : public MotorDriverController {
+private:
     int lmf, lmb, rmf, rmb;
 
     int enl, enr;
 
     String status;
 
-    public:
+public:
     /// @brief Constuctor initializing the L298Controller.
     /// @param leftForwardPin Pin for left motor forward direction
     /// @param leftBackwardPin Pin for left motor backward direction
@@ -37,7 +80,7 @@ class L298Controller {
         int rightBackwardPin = 7,
         int enableLeftPin = 6,
         int enableRightPin = 5
-        ) {
+    ) {
         // Set pin numbers
         lmf = leftForwardPin;
         lmb = leftBackwardPin;
@@ -58,7 +101,7 @@ class L298Controller {
 
     /// MOVEMENT FUNCTION --> Left
     /// @param speed Speed of the right motor. Range: 0-255. Default: 255
-    void smoothLeft(int speed = 255) {
+    void smoothLeft(int speed = 255) override {
         digitalWrite(lmf, 0);
         digitalWrite(lmb, 0);
         digitalWrite(rmf, 1);
@@ -69,7 +112,7 @@ class L298Controller {
 
     /// MOVEMENT FUNCTIONS --> Right
     /// @param speed Speed of the left motor. Range: 0-255. Default: 255
-    void smoothRight(int speed = 255) {
+    void smoothRight(int speed = 255) override {
         digitalWrite(lmf, 1);
         digitalWrite(lmb, 0);
         digitalWrite(rmf, 0);
@@ -80,7 +123,7 @@ class L298Controller {
 
     /// MOVEMENT FUNCTIONS --> On-Spot Left
     /// @param speed Speed of the right motor forward and left motor reversed. Range: 0-255. Default: 255
-    void hardLeft(int speed = 255) {
+    void hardLeft(int speed = 255) override {
         digitalWrite(lmf, 0);
         digitalWrite(lmb, 1);
         digitalWrite(rmf, 1);
@@ -92,7 +135,7 @@ class L298Controller {
 
     /// MOVEMENT FUNCTIONS --> On-Spot Right
     /// @param speed Speed of the left motor forward and right motor reversed. Range: 0-255. Default: 255
-    void hardRight(int speed = 255) {
+    void hardRight(int speed = 255) override {
         digitalWrite(lmf, 1);
         digitalWrite(lmb, 0);
         digitalWrite(rmf, 0);
@@ -104,7 +147,7 @@ class L298Controller {
 
     /// MOVEMENT FUNCTIONS --> Forward
     /// @param speed Speed of both motors. Range: 0-255. Default: 255
-    void forward(int speed = 255) {
+    void forward(int speed = 255) override {
         digitalWrite(lmf, 1);
         digitalWrite(lmb, 0);
         digitalWrite(rmf, 1);
@@ -114,9 +157,9 @@ class L298Controller {
         status = "Moving Forward!";
     }
 
-    /// MOVEMENT FUNCTIONS --> Back
+    /// MOVEMENT FUNCTIONS --> Backward
     /// @param speed Speed of both motors in reverse. Range: 0-255. Default: 255
-    void back(int speed = 255) {
+    void backward(int speed = 255) override {
         digitalWrite(lmf, 0);
         digitalWrite(lmb, 1);
         digitalWrite(rmf, 0);
@@ -125,7 +168,7 @@ class L298Controller {
     }
 
     /// MOVEMENT FUNCTIONS --> Stop
-    void stop() {
+    void stop() override {
         digitalWrite(lmf, 0);
         digitalWrite(lmb, 0);
         digitalWrite(rmf, 0);
@@ -133,13 +176,5 @@ class L298Controller {
         analogWrite(enl, 0);
         analogWrite(enr, 0);
         status = "Stopped!";
-    }
-
-    /// GETTER FUNCTION --> Status
-    /// @return [String] status of the bot
-    /// @param verbose [bool] if true, prints the status of the motor driver in Serial.
-    String getStatus(bool verbose = false) {
-        if (verbose) Serial.println(status);
-        return status;
     }
 };
